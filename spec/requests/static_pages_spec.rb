00d903b291aa84527b5d887382b33b23a9fe75db
secondrocker <349676126@qq.com>
2014-11-29 22:03:58 +0800

    mini modify

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "Static pages" do
   subject{page}
   describe "Home page" do
     before{visit home_path}
-    
+
     it{ should have_content("Sample App") }
     it{ should have_title("#{base_title}") }
     it{ should_not have_title("Home") }
@@ -13,20 +13,21 @@ describe "Static pages" do
 
   describe "Help page" do
     before{visit help_path}
-    
+
     it{ should have_content("Help") }
     it{ should have_title("#{base_title} | Help") }
   end
   describe "About page" do
     before{visit about_path}
-    
+
     it{ should have_content("About Us") }
     it{ should have_title("#{base_title} | About Us") }
   end
 
   describe "Contact Us" do
     before{visit contact_path}
-    
+
+    it{should have_content("Contact")}
     it{ should have_content("Contact Us") }
     it{ should have_title("#{base_title} | Contact Us") }
   end
